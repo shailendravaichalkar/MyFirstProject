4528c8b136e27b9afda0c0ee5c050e7ba8378e9f
Shailendra Vaichalkar <vaichalkar.shailendra@gmail.com>
2020-03-07 09:48:44 +0530

    try 1

--- buildjenkins.sh
+++ buildjenkins.sh
@@ -1,1 +1,5 @@
+docker build -t myownnginx:v1 .
+docker container stop myownnginxImage 
+docker container rm myownnginxImage
+docker run --name myownnginxImage -d -p 9090:80 myownnginx:v1
 curl 'http://localhost:8080/job/MyFirstJenkinsJob/build?token=shailendra'
